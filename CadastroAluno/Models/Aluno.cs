417dfaab35ae908fb2c54c15eb1a25428e1441c1
fezokaa <fezokaa@yahoo.com>
2022-09-29 21:47:54 -0300

    Criação do Modelo (Aluno)

---
+++ CadastroAluno/Models/Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroAluno.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Turma { get; set; }
+
+        public AtualizarDados(string nome,string turma)
+        {
+            Nome = nome;
+            turma = turma;
+        }
+    }
+}
