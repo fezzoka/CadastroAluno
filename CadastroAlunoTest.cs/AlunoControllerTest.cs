e66331b661d13f662cc4dc257411d610e5a536f3
fezokaa <fezokaa@yahoo.com>
2022-10-05 21:39:14 -0300

    model

--- CadastroAlunoTest.cs/AlunoControllerTest.cs
+++ CadastroAlunoTest.cs/AlunoControllerTest.cs
@@ -9,16 +9,24 @@ namespace CadastroAlunoTest.cs
 {
     public class AlunoControllerTest
     {
-        private Mock<IAlunoRepository> _mockAlunoController;
-        private AlunosController _controller;
+        Mock<IAlunoRepository> _mockAlunoController;
+         AlunosController _controller;
 
         public AlunoControllerTest()
         {
             _mockAlunoController = new Mock<IAlunoRepository>();
             _controller = new AlunosController(_mockAlunoController.Object);
         }
-       
+        [Fact]
+        public void ChamaORepositorioUmaVezComOuSemRegistro()
+        {
 
+            //arrange
+            var result = _controller.Index();
+            //act
+            //assert
+
+        }
     }
 }
 
